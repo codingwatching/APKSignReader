71742f5b8ffc727e3d1e7eafc3e80ccfb05e3a8e
kur0yama <41464808+kur0yama@users.noreply.github.com>
2022-04-14 21:53:48 +0800

    Initial Commit

---
+++ Windows/APKSignReader/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace APKSignReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(562, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "(DRAG APK HERE OR USE THE OPEN BUTTON)";
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(620, 279);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Result";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.richTextBox2);
+            this.groupBox2.Location = new System.Drawing.Point(6, 124);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(608, 150);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Result in Format:";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(125, 21);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(49, 19);
+            this.radioButton3.TabIndex = 17;
+            this.radioButton3.Text = "C++";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(73, 21);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(46, 19);
+            this.radioButton2.TabIndex = 16;
+            this.radioButton2.Text = "Hex";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(61, 19);
+            this.radioButton1.TabIndex = 15;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Base64";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(6, 48);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(596, 96);
+            this.richTextBox2.TabIndex = 14;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(608, 96);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(580, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Android Application|*.apk";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 338);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "APK Sign Reader";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ Windows/APKSignReader/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography.Pkcs;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.IO.Compression;
+
+namespace APKSignReader
+{
+    public partial class Form1 : Form
+    {
+        private SignedCms signedCms;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files != null && files.Length != 0)
+            {
+                textBox1.Text = files[0];
+            }
+        }
+
+        private void textBox1_DragLeave(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.None;
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                e.Effect = DragDropEffects.All;
+
+                parseSignatures();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                parseSignatures();
+            }
+        }
+        private void parseSignatures()
+        {
+            try
+            {
+                using (ZipArchive archive = ZipFile.OpenRead(textBox1.Text))
+                {
+                    foreach (var entry in archive.Entries)
+                    {
+                        if (entry.Name.Contains(".DSA") || entry.Name.Contains(".RSA"))
+                        {
+                            byte[] b;
+                            using (Stream stream = entry.Open())
+                            {
+                                using (var ms = new MemoryStream())
+                                {
+                                    stream.CopyTo(ms);
+                                    b = ms.ToArray();
+                                }
+                            }
+
+                            signedCms = new SignedCms();
+                            signedCms.Decode(b);
+
+                            richTextBox1.Text = "";
+                            foreach (SignerInfo signerInfo in signedCms.SignerInfos)
+                            {                          
+                                X509Certificate2 cert = new X509Certificate2(signerInfo.Certificate.RawData);
+                                richTextBox1.Text += "Signer: " + cert.Subject + "\r\n";
+                                richTextBox1.Text += "Signature Algorithm: " + signerInfo.DigestAlgorithm.FriendlyName + "\r\n";
+                                richTextBox1.Text += "Signature: " + BitConverter.ToString(signerInfo.GetSignature()) + "\r\n";
+                                richTextBox1.Text += "\r\n";
+                            }
+
+                            formatSignatures();
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBox2.Text = "Not a valid APK file!";
+            }
+        }
+        
+        private void formatSignatures()
+        {
+            var certs = signedCms.Certificates;
+
+            if (radioButton1.Checked) {
+                Stream stream = new MemoryStream();
+                BinaryWriter writer = new BinaryWriter(stream);
+
+                writer.Write(certs.Count);
+                foreach (var cert in certs)
+                {
+                    writer.Write(cert.RawData.Length);
+                    writer.Write(cert.RawData);
+                }
+
+                byte[] bytes = new byte[stream.Length];
+                stream.Position = 0;
+                stream.Read(bytes, 0, (int)stream.Length);
+
+                richTextBox2.Text = Convert.ToBase64String(bytes);
+                writer.Close();
+                stream.Close();
+            }
+            if (radioButton2.Checked) {
+                Stream stream = new MemoryStream();
+                BinaryWriter writer = new BinaryWriter(stream);
+
+                writer.Write(certs.Count);
+                foreach (var cert in certs)
+                {
+                    writer.Write(cert.RawData.Length);
+                    writer.Write(cert.RawData);
+                }
+
+                byte[] bytes = new byte[stream.Length];
+                stream.Position = 0;
+                stream.Read(bytes, 0, (int)stream.Length);
+
+                richTextBox2.Text = BitConverter.ToString(bytes).Replace("-", " ");
+                writer.Close();
+                stream.Close();
+            }
+            if (radioButton3.Checked)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("std::vector<std::vector<uint8_t>> apk_signatures {");
+                for (int i = 0; i < certs.Count; i++)
+                {
+                    sb.Append("{");
+                    byte[] cert = certs[i].RawData;
+                    for (int j = 0; j < cert.Length; j++)
+                    {
+                        sb.Append("0x" + cert[j].ToString("X2"));
+                        if (j != cert.Length - 1)
+                        {
+                            sb.Append(", ");
+                        }
+                    }
+                    sb.Append("}");
+                    if (i != certs.Count - 1)
+                    {
+                        sb.Append(", ");
+                    }
+                }
+                sb.Append("};");
+                richTextBox2.Text = sb.ToString();
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            formatSignatures();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            formatSignatures();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            formatSignatures();
+        }
+    }
+}
